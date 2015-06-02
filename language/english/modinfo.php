8e5281ef1f0e4e4080d3375f8e8b736d79e3a460
mambax7 <mambax7@gmail.com>
2015-06-02 02:32:48 -0400

    Adding English files

---
+++ language/english/modinfo.php
@@ -0,0 +1,22 @@
+<?php
+//  ------------------------------------------------------------------------ //
+// 本模組由 tad 製作
+// 製作日期：2014-06-17
+// $Id:$
+// ------------------------------------------------------------------------- //
+include_once XOOPS_ROOT_PATH . "/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+
+define('_MI_TADGUIDE_NAME', 'Tad Guide');
+define('_MI_TADGUIDE_AUTHOR', 'Tad');
+define('_MI_TADGUIDE_CREDITS', '');
+define('_MI_TADGUIDE_DESC', 'This module is used to quickly create a group and open the corresponding classification and setting authority in each module');
+define('_MI_TADGUIDE_AUTHOR_WEB', 'Tad teaching network');
+define('_MI_TADGUIDE_ADMENU1', 'Manager View');
+define('_MI_TADGUIDE_ADMENU1_DESC', 'Master Management Interface');
+
+define('_MI_TADGUIDE_DIRNAME', basename(dirname(dirname(__DIR__))));
+define('_MI_TADGUIDE_HELP_HEADER', __DIR__ . '/help/helpheader.html');
+define('_MI_TADGUIDE_BACK_2_ADMIN', 'Back to Administration of ');
+
+//help
+define('_MI_TADGUIDE_HELP_OVERVIEW', 'Overview');
