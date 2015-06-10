0be5ef2c1323f39f544bed8622ff384f149f7300
Tad <tad0616@gmail.com>
2015-06-10 14:31:28 +0800

    調整語系

--- language/tchinese_utf8/modinfo.php
+++ language/tchinese_utf8/modinfo.php
@@ -4,14 +4,12 @@
 // 製作日期：2014-06-17
 // $Id:$
 // ------------------------------------------------------------------------- //
-include_once XOOPS_ROOT_PATH."/modules/tadtools/language/{$xoopsConfig['language']}/modinfo_common.php";
+include_once XOOPS_ROOT_PATH . '/modules/tadtools/language/' . $xoopsConfig['language'] . '/modinfo_common.php';
 
-define('_MI_TADGUIDE_NAME','XOOPS安裝精靈');
-define('_MI_TADGUIDE_AUTHOR','tad');
-define('_MI_TADGUIDE_CREDITS','');
-define('_MI_TADGUIDE_DESC','此模組用來快速建立群組以及在各模組開設對應的分類及設定權限');
-define('_MI_TADGUIDE_AUTHOR_WEB','Tad 教材網');
+define('_MI_TADGUIDE_NAME', 'XOOPS安裝精靈');
+define('_MI_TADGUIDE_AUTHOR', 'tad');
+define('_MI_TADGUIDE_CREDITS', '');
+define('_MI_TADGUIDE_DESC', '此模組用來快速建立群組以及在各模組開設對應的分類及設定權限');
+define('_MI_TADGUIDE_AUTHOR_WEB', 'Tad 教材網');
 define("_MI_TADGUIDE_ADMENU1", "主管理介面");
 define("_MI_TADGUIDE_ADMENU1_DESC", "主管理介面");
-
-?>
