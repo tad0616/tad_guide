d73d6b08d4bd60ea9c6638c201a41a90f7fc720f
Tad <tad0616@gmail.com>
2018-12-29 18:15:08 +0800

    修改預設值

--- admin/setup/tad_login/tchinese_utf8/config.php
+++ admin/setup/tad_login/tchinese_utf8/config.php
@@ -5,7 +5,7 @@ $new_config['google_appId']        = '';
 $new_config['google_secret']       = '';
 $new_config['google_redirect_uri'] = '';
 $new_config['google_api_key']      = '';
-$new_config['auth_method']         = 'a:4:{i:0;s:5:"yahoo";i:1;s:3:"chc";i:2;s:3:"cyc";i:3;s:2:"tn";}';
+$new_config['auth_method']         = 'a:4:{i:0;s:2:"tc";i:1;s:3:"chc";i:2;s:3:"cyc";i:3;s:2:"tn";}';
 $new_config['openid_login']        = '1';
 $new_config['openid_logo']         = '4';
 
