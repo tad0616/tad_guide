6900a6a0c153ad56cd26395c815886b391f45330
Tad <tad0616@gmail.com>
2019-04-30 12:46:02 +0800

    加語系

--- language/tchinese_utf8/main.php
+++ language/tchinese_utf8/main.php
@@ -1,3 +1,3 @@
 <?php
-//需加入模組語系
+xoops_loadLanguage('main', 'tadtools');
 define('_TAD_NEED_TADTOOLS', '需要 tadtools 模組，可至<a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS輕鬆架</a>下載。');
