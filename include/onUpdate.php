587dcc4fb99a00a4b37451f3f42f91b7a2d781a2
Tad <tad0616@gmail.com>
2019-05-10 16:24:46 +0800

    修改說明

--- include/onUpdate.php
+++ include/onUpdate.php
@@ -2,6 +2,9 @@
 
 use XoopsModules\Tad_guide\Update;
 
+if (!class_exists('XoopsModules\Tad_guide\Update')) {
+    include dirname(__DIR__) . '/preloads/autoloader.php';
+}
 function xoops_module_update_tad_guide(&$module, $old_version)
 {
     return true;
